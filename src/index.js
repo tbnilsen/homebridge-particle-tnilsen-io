cc9d791c2ffa6db728fe9351756884c91c493150
TERJE B NILSEN <43384040+tbnilsen@users.noreply.github.com>
2020-12-02 16:28:42 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const ParticlePlatform = require('./Platform.js');
 
 module.exports = (homebridge) => {
   global.homebridge = homebridge;
-  homebridge.registerPlatform('homebridge-particle-io', 'ParticleIO', ParticlePlatform);
+  homebridge.registerPlatform('homebridge-particle-tnilsen-io', 'ParticleIO', ParticlePlatform);
 };
