fad7ce5fec9446babdcd10ee129b7d402db507b9
TERJE B NILSEN <43384040+tbnilsen@users.noreply.github.com>
2020-12-11 07:12:48 -0600

    Update StatelessProgrammableSwitchAccessory.js

--- src/StatelessProgrammableSwitchAccessory.js
+++ src/StatelessProgrammableSwitchAccessory.js
@@ -1,1 +1,17 @@
-ProgrammableSwitchEvent
+onst ActorAccessory = require('./StatelessProgrammableSwitchAccessory.js');
+
+class StatelessProgrammableSwitchAccessory extends ActorAccessory {
+
+  constructor(log, url, accessToken, device, homebridge) {
+    const Service = homebridge.hap.Service;
+    const Characteristic = homebridge.hap.Characteristic;
+    super(log, url, accessToken, device, homebridge, Service.StatelessProgrammableSwitch, Characteristic.ProgrammableSwitchEvent);
+  }
+
+  setState(value, callback) {
+    super.setState(value ? '1' : '0', callback);
+  }
+}
+
+module.exports = SwitchAccessory;
+
